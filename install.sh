3638f53129b85a6f495cd469e418b84dd6af1d0a
Artur M. Brodzki <artur@brodzki.org>
2020-04-18 10:43:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+set -e
+
 git clone https://github.com/ArturB/brodzki-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp brodzki-zsh/brodzki.zsh-theme ~/.oh-my-zsh/themes/
 sed -i 's/^ZSH_THEME=\".*/ZSH_THEME=\"brodzki\"/g'
 rm -r brodzki-zsh
-
-
