c407e4e3afa4a65b3649b617b8bf691ed51de740
Artur M. Brodzki <artur@brodzki.org>
2020-07-30 10:23:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,9 +18,9 @@ if [[ -d .oh-my-zsh ]] ; then
 fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-cp brodzki-zsh/brodzki.zsh-theme ~/.oh-my-zsh/themes/
-sed -i 's/^ZSH_THEME=\".*/ZSH_THEME=\"brodzki\"/g' ~/.zshrc
-sed -i 's/.*COMPLETION_WAITING_DOTS.*/COMPLETION_WAITING_DOTS=\"true\"/g' ~/.zshrc
+cp brodzki-zsh/brodzki.zsh-theme .oh-my-zsh/themes/
+sed -i 's/^ZSH_THEME=\".*/ZSH_THEME=\"brodzki\"/g' .zshrc
+sed -i 's/.*COMPLETION_WAITING_DOTS.*/COMPLETION_WAITING_DOTS=\"true\"/g' .zshrc
 
 rm -rf brodzki-zsh
 exit 0
